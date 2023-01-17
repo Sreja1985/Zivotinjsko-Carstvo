79d584c313a5f970e55784cc940e012ead9925b3
Sreja1985 <sreckoljubic85@gmail.com>
2023-01-17 20:03:05 +0100

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -39,7 +39,7 @@ function Footer() {
                 <Link className='links' to='/'>Milksy keks 20g</Link>
                 <Link className='links' to='/'>Lizli 15g</Link>
                 <Link className='links' to='/'>Milksy Kraš Express 20g</Link>
-		        <Link className='links' to='/'>Album</Link>
+		<Link className='links' to='/'>Album</Link>
             </div>
             <div>
                 <h4>Životinje</h4>

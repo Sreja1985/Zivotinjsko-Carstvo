94e333dfe32e753c29a0ac80fd2f90daf9066113
Sreja1985 <sreckoljubic85@gmail.com>
2023-02-08 19:30:37 +0100

    Životinjsko Carstvo

---
+++ src/Components/AnimalGallery/ArthropodsGallery.js
@@ -0,0 +1,24 @@
+export const ArthropodsGallery = [
+    {
+        url:'https://cdn.britannica.com/67/7267-004-4FD7E434/Centipede.jpg'
+    },
+    {
+        url:'https://cdn.britannica.com/35/140735-050-57280DEF/spiny-lobster.jpg'
+    },
+    {
+        url:'https://zrnoznanja.com/wp-content/uploads/2018/05/leptir-monarh.jpg'
+    },
+    {
+        url:'https://cdn.britannica.com/97/46897-004-E3937032/eyes-horseshoe-crab-myriapods-trilobites-chelicerates-scorpions.jpg?s=1500x700&q=85'
+    },
+    {
+        url:'https://cdn.britannica.com/48/202548-050-F6FA9D20/millipede-South-Africa.jpg'
+    },
+    {
+        url:'https://cdn.britannica.com/05/9305-050-B308B8B1/Spider-crab.jpg'
+    },
+    {
+        url:'https://cdn.britannica.com/94/123294-050-E3995FA0/Crayfish-crustaceans-crabs-lobsters-feature-pinching-claws.jpg'
+    },
+    
+]
---
+++ src/Components/AnimalGallery/EchinodermsGallery.js
@@ -0,0 +1,17 @@
+export const EchinodermsGallery = [
+    {
+        url:'https://exrja3g2wej.exactdn.com/wp-content/uploads/seaurchin2.jpg?strip=all&lossy=1&resize=750%2C476&ssl=1'
+    },
+    {
+        url:'https://dalibor-andres.from.hr/uw/slike_v/jbo_016-1_600.jpg'
+    },
+    {
+        url:'https://www.fiuman.hr/wp-content/uploads/2020/10/9B14E4EB-582E-4813-90CD-A666654BF332-900x600.jpeg'
+    },
+    {
+        url:'https://dalibor-andres.from.hr/uw/slike_v/jbo_018-1_600.jpg'
+    },
+    {
+        url:'https://static.slobodnadalmacija.hr/images/slike/2021/07/17/18601134.jpg'
+    },
+]
